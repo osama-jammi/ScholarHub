00dd9ee86d6b71ce71833687840a4742f7d59935
issam-mouhala <166968346+issam-mouhala@users.noreply.github.com>
2025-03-26 10:57:17 +0000

    aouter les fichier structurer pour chaque partie de site{index.html,script.js,index.php,style.css}

--- ScholarHub/public/js/main.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('theme-toggle').addEventListener('click', function () {
-    document.body.classList.toggle('dark-mode');
-    this.textContent = document.body.classList.contains('dark-mode') ? '☀️' : '🌙';
-});
